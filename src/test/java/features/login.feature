064e82a286a6210bbcd7fa65817c790ce246aa34
kambulen <kambulen@gmail.com>
2024-05-10 11:54:39 +0200

    Add files via upload

---
+++ src/test/java/features/login.feature
@@ -0,0 +1,11 @@
+Feature: Login
+
+  Scenario Outline: As a user I want to login and verify if the login is successful
+    Given I am on the login page
+    And I enter the correct username <username>
+    And I enter the correct password <password>
+    When I click on the login button
+    Then The I get directed to the home page
+    Examples:
+      | username      | password     |
+      | standard_user | secret_sauce |
---
+++ src/test/java/steps/stepDefinition.java
@@ -0,0 +1,46 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class stepDefinition {
+
+    WebDriver driver;
+
+    @Given("I am on the login page")
+    public void i_am_on_the_login_page() {
+
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+    }
+
+    @And("^I enter the correct username (.*)$")
+    public void i_enter_the_correct_username_username(String username) {
+        driver.findElement(By.id("user-name")).sendKeys(username);
+
+    }
+
+    @And("^I enter the correct password (.*)$")
+    public void i_enter_the_correct_password_password(String password) {
+        driver.findElement(By.id("password")).sendKeys(password);
+
+    }
+
+    @When("I click on the login button")
+    public void i_click_on_the_login_button() {
+        driver.findElement(By.id("login-button")).click();
+
+    }
+
+    @Then("The I get directed to the home page")
+    public void the_i_get_directed_to_the_home_page() {
+
+    }
+}
