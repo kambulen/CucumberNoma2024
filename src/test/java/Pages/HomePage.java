bf9b354cb3a2464f6ea5b04beca7c1b15b3825c5
Noma <nomathemba.nondzaba@smokeccs.com>
2024-05-18 21:20:58 +0200

    Created the Login and home pages Updated dependencies

---
+++ src/test/java/Pages/HomePage.java
@@ -0,0 +1,34 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class HomePage {
+
+    public WebDriver driver;
+
+    @FindBy(xpath = "//span[contains(text(),'Products')]")
+    WebElement product_xpath;
+    @FindBy(xpath = "//button[@name='add-to-cart-sauce-labs-backpack']")
+    WebElement addToCartButton_xpath;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyUserIsLoggedIn() {
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(product_xpath));
+//        product_xpath.isDisplayed();
+        String productText = product_xpath.getText();
+        Assert.assertEquals(productText,"Products");
+
+    }
+
+
+}
