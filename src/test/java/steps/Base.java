7666003cb0263bd7b95708a8589781a12e5bd4b4
Noma <nomathemba.nondzaba@smokeccs.com>
2024-05-21 15:40:03 +0200

    Created the Base class

---
+++ src/test/java/steps/Base.java
@@ -0,0 +1,15 @@
+package steps;
+
+import Helpers.BrowserFactory;
+import Pages.HomePage;
+import Pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class Base {
+BrowserFactory browserFactory = new BrowserFactory();
+final WebDriver driver = browserFactory.startBrowser("Chrome","https://www.saucedemo.com/" );
+LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+
+}
