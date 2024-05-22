a3495ca5cfe651d0b78b0b7322737065df92b927
Noma <nomathemba.nondzaba@smokeccs.com>
2024-05-22 14:23:26 +0200

    enabled headless broswer BroswerFactory

--- src/test/java/Helpers/BrowserFactory.java
+++ src/test/java/Helpers/BrowserFactory.java
@@ -3,6 +3,7 @@ package Helpers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -13,8 +14,10 @@ public class BrowserFactory {
 
     public static WebDriver startBrowser(String browserChoice, String url) {
         if ("chrome".equalsIgnoreCase(browserChoice)) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
         } else if ("firefox".equalsIgnoreCase(browserChoice)) {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
